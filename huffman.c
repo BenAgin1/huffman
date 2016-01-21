66a628b38fe2dbbce936e4deb0ccfdfdf83fe840
Lorenz Gerber <genfys@gmail.com>
2016-01-21 22:25:28 +0100

    compiles

---
+++ huffman.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    // Handling arg input
+
+    // read template text
+
+    // frequency analysis
+
+    // freq array of length 255
+
+    // loop trough text and cast each symbol to int
+    // increase the index == int by one
+
+    // find lowest two values in freq array and list of trees
+
+    // build new tree in list of tree either from two values of freq list
+    // or 1 value freq list and one value tree list or two value tree list
+    // all values from the tree list require rebuilding the tree and then removing
+    // the old tree, until just one tree in tree list
+
+
+
+
+
+    printf("Hello, World!\n");
+    return 0;
+}
